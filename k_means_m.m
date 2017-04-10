4d0384756ee8bc5a21d7f7b04818415cb33548b4
Yichen Qian <92yichenqian@tongji.edu.cn>
2017-04-10 22:17:29 +0800

    update

--- k_means_m.m
+++ k_means_m.m
@@ -12,49 +12,81 @@ n = 100;
 load('input_100_2.mat');
 [~, n] = size(A); %num of nodes
 times = 0;
-k = 10; %num of clusters
+k = 2; %num of clusters
+rounds = 1;
+round = 0;
+min_sum = inf;
+min_result = [];
+f_ch = [];
 
-cluster_head = unidrnd(n,1,k);
-first = cluster_head;
-%cluster_head = [2,7];
-result = zeros(n,1); %clustering result
-history_c = zeros(1,k); %the last clustering result
-num = zeros(1,k); %num of nodes in a cluster
-
-%K-Means
-while (isequal(history_c,cluster_head)==0 && times<=n^2)
-    history_c = cluster_head;
-    %Clustering
-    [min_v,result] = min(A(:,cluster_head),[],2);
-    for i = 1:k
-        num(i) = length(find(result == i));
+%random generate the graph
+A = inf(n,n);
+for i = 1:n
+    connect = unidrnd(n,1,5);
+    for j = 1:length(connect)
+        A(i,connect(j)) = unidrnd(20);
+        A(connect(j),i) = unidrnd(20);
     end
-    %Load banlancing
-    for i = 1:n
-        min_s = find(A(i,cluster_head) == min_v(i));
-        if length(min_s)>1
-             [~,pos] = min(num(min_s));
-             num(result(i)) = num(result(i)) - 1;
-             result(i) = pos;
-             num(pos) = num(pos) + 1;
+    A(i,i) = 0;
+end
+
+% for i = 1:n
+%     for j = 1:n
+%         [A(i,j),~] = dijkstra(A,i,j);
+%     end
+% end
+
+while (round < rounds)
+    cluster_head = unidrnd(n,1,k);
+    first = cluster_head;
+    cluster_head = [2,7];
+    result = zeros(n,1); %clustering result
+    history_c = zeros(1,k); %the last clustering result
+    num = zeros(1,k); %num of nodes in a cluster
+    
+    %K-Means
+    while (isequal(history_c,cluster_head)==0 && times<=n^2)
+        history_c = cluster_head;
+        %Clustering
+        [min_v,result] = min(A(:,cluster_head),[],2);
+        for i = 1:k
+            num(i) = length(find(result == i));
         end
+        %Load banlancing
+        for i = 1:n
+            min_s = find(A(i,cluster_head) == min_v(i));
+            if length(min_s)>1
+                [~,pos] = min(num(min_s));
+                num(result(i)) = num(result(i)) - 1;
+                result(i) = pos;
+                num(pos) = num(pos) + 1;
+            end
+        end
+        
+        %Finding cluster head
+        for i = 1:k
+            index = find(result == i);
+            [~,pos] = min(sum(A(index,index)));
+            cluster_head(i) = index(pos);
+        end
+        times = times + 1;
     end
     
-    %Finding cluster head
-    for i = 1:k
-        index = find(result == i);
-        [~,pos] = min(sum(A(index,index)));
-        cluster_head(i) = index(pos);
+    %Find the nearest black node
+    
+    sum_of_weight = 0;
+    for i =1:k
+        sum_of_weight = sum_of_weight + sum(A(result == i,cluster_head(i)));
     end
-    times = times + 1;
+    
+    if sum_of_weight < min_sum
+        min_result = result;
+        f_ch = cluster_head;
+    end
+    round = round + 1;
 end
 
-sum_of_weight = 0;
-for i =1:k
-    sum_of_weight = sum_of_weight + sum(A(result == i,cluster_head(i)));
-end
-
-result
+min_result
 first
-cluster_head
-sum_of_weight
+f_ch
+min_sum
